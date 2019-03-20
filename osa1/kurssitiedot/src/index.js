fc227ce4621c26af864bd1b7aa01587fe83b5e28
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-03-20 02:59:51 +0200

    osa 1.1

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return (
+        <h1>{props.name}</h1>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Yhteensä {props.count}</p>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <p>{props.part} {props.num}</p>
+    )
+}
+
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys'
+  const part1 = 'Reactin perusteet'
+  const exercises1 = 10
+  const part2 = 'Tiedonvälitys propseilla'
+  const exercises2 = 7
+  const part3 = 'Komponenttien tila'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course}/>
+      
+      <Content part={part1} num={exercises1}/>
+      <Content part={part2} num={exercises2}/>
+      <Content part={part3} num={exercises3}/>
+      
+      <Total count={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

2d64fa718b67d879dba7228a35ada67d5a3f6474
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-03-21 13:28:00 +0200

    unicafe step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,38 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ text }) => <div>{text}</div>
-
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [ugly, setUgly] = useState(0)
 
   return (
     <div>
-      <Display text='<h2>anna palautetta</h2>'/>
+      <h3>Anna palautetta</h3>
+      <div>
+        <button onClick={() => setGood(good + 1)}>
+          hyvä
+        </button>
+        <button onClick={() => setUgly(ugly + 1)}>
+          neutraali
+        </button>
+        <button onClick={() => setBad(bad + 1)}>
+          huono
+        </button>
+      </div>
+      <div>
+        <h3>Statistiikka</h3>
+          <p>
+            hyvä: {good}
+          </p>
+          <p>
+            neutraali: {ugly}
+          </p>
+          <p>
+            huono: {bad}
+          </p>
+      </div>
     </div>
   )
 }

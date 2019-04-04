0beb3dab763de1a5dbbabf8b73c30031775bcbca
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-04-04 13:27:42 +0300

    2.4: kurssitiedot step9

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -2,34 +2,61 @@ import React from 'react'
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-				exercises: 10,
-				id: 1
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-				exercises: 7,
-				id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-				exercises: 14,
-				id: 3
-			},
-			{
-        name: 'Redux',
-				exercises: 7,
-				id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  console.log('App toimii..')
+  console.log(courses)
+
+  const elements = () => courses.map(course =>
+    <Course
+      key={course.id}
+      course={course}
+    />
+  )
+
 
   return (
-		<Course course={course} />
+    <div>
+      {elements()}
+    </div>
   )
 }
 

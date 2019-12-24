748c306558d2529e24c27c703777c14e9d4c3585
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-12-24 15:55:24 +0200

    5.1: blogilistan frontend, step1

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(blogit => setBlogs(blogit))
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      window.localStorage.setItem(
+        'loggedinUser', JSON.stringify(user)
+      )
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log('bad credentials')
+    }
+  }
+
+  const blogForm = () => {
+    return (
+      <section>
+        <p>{user.name} logged in</p>
+        <button>logout</button>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </section>
+    )
+  }
+
+  const loginForm = () => {
+    return (
+      <fieldset>
+        <legend>log in to application</legend>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              value={username}
+              id="username"
+              name="username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              value={password}
+              id="password"
+              name="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+
+          <button type="submit">login</button>
+        </form>
+      </fieldset>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Blogilista</h1>
       </header>
+
+      {user === null && loginForm()}
+      {user !== null && blogForm()}
+
     </div>
-  );
+  )
 }
 
 export default App;

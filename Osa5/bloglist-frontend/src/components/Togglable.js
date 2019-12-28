02558391c30589e68caebed780b9dcc19afdfbad
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-12-28 22:45:58 +0200

    5.11: blogilistan frontend, step11

--- Osa5/bloglist-frontend/src/components/Togglable.js
+++ Osa5/bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React, { useState, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
@@ -19,14 +20,23 @@ const Togglable = React.forwardRef((props, ref) => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        <button onClick={toggleVisibility}>
+          {props.buttonLabel}
+        </button>
       </div>
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility}>cancel</button>
+        <button onClick={toggleVisibility}>
+          cancel
+          </button>
       </div>
     </div>
   )
 })
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+  // className: PropTypes.string.isRequired
+}
+
 
 export default Togglable
